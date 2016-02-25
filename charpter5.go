2786008d73ba04805c772a9166dee4c0e21c4cf5
wangtongphp <1006440989@qq.com>
2016-02-25 17:32:41 +0800

    fmt charpter5.go

--- charpter5.go
+++ charpter5.go
@@ -1,39 +1,38 @@
 package main
 
-import(
-    "os"
-    "io"
-    "net/http"
-    "log"
-    "fmt"
-    )
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
 
-func main(){
-    //httpT()
-    webT()
+func main() {
+	//httpT()
+	webT()
 }
 
-func webT(){
-    http.HandleFunc("/h",hellowHandler)
-    err := http.ListenAndServe(":8080", nil)
-    if err != nil {
-        log.Fatal("ListenAndServe: ", err.Error())
-    }
+func webT() {
+	http.HandleFunc("/h", hellowHandler)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err.Error())
+	}
 }
 
-
-func hellowHandler(w http.ResponseWriter, r *http.Request){
-    io.WriteString(w, "hello world")
+func hellowHandler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "hello world")
 }
 
-func httpT(){
-    req, err := http.NewRequest("GET","http://www.mi.com", nil)
-    req.Header.Add("User-Agent", "wangtong-PC")
-    client := &http.Client{}
-    resp, err := client.Do(req)
-    if err != nil {
-        fmt.Println(err)
-    }
-    io.Copy(os.Stdout, resp.Body)
-    fmt.Println(resp)
+func httpT() {
+	req, err := http.NewRequest("GET", "http://www.mi.com", nil)
+	req.Header.Add("User-Agent", "wangtong-PC")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+	}
+	io.Copy(os.Stdout, resp.Body)
+	fmt.Println(resp)
 }
