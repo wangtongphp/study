15929defada87f2fb23cfbadbb1df9090bc8b55b
wangtongphp <1006440989@qq.com>
2016-03-23 21:20:36 +0800

    log

---
+++ log.go
@@ -0,0 +1,52 @@
+package main
+
+import(
+    "log"
+    "bytes"
+    "fmt"
+    "os"
+    "time"
+    "github.com/Sirupsen/logrus"
+)
+
+func main(){
+    logrusT()
+    logT()
+}
+
+
+func logrusT(){
+    logrus.SetLevel(logrus.DebugLevel)
+    logr := logrus.New()
+    logr.Debug("debuggg")
+    logr.Info("this is info")
+    time.Sleep(1e9)
+    logr.Warn("warning")
+    logr.Error("errorrr")
+
+    logrus.SetFormatter(&logrus.JSONFormatter{})
+    logrus.Info("JSONFormatter")
+
+    logrus.SetFormatter(&logrus.TextFormatter{})
+    logrus.Info("TextFormatter")
+
+
+    //logr.Fatal(logrus.GetLevel())
+}
+
+func logT(){
+    logger := log.New(os.Stdout, "logger: ", log.Lshortfile)
+    logger.Print("Hello, log file!")
+    time.Sleep(1e9)
+    logger.Print("\033[1;4;32m Hello, logiiiii file! \033[0m")
+
+    var buf bytes.Buffer
+    logger = log.New(&buf, "logger: ", log.Lshortfile)
+    logger.Print("Hello, log file!")
+    time.Sleep(1e9)
+    logger.Print("Hello, logiiiii file!")
+    fmt.Print(&buf)
+
+}
+
+
