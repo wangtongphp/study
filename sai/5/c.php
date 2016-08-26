d25d93aa17f46900e5b43f93031bc0091be73221
wangtongphp <1006440989@qq.com>
2016-08-26 19:54:34 +0800

    c

--- sai/5/c.php
+++ sai/5/c.php
@@ -13,13 +13,13 @@ for($g=0; $g<$group; $g++){
     list($num, $line, $ask) = explode(' ',$f[$k++]);
     for($l=0;$l<$line;$l++){
         $p = explode(' ',$f[$k++]);
-        $res[$p[0]] = $num-$p[1];
+        $res[$p[0]] = '-'.$p[1];
         if($res[$p[0]]<0){
             echo  'liar';
             echo PHP_EOL;
             break;
         }
-        $res[$p[0]+$p[2]] = $num+$p[1];
+        $res[$p[0]+$p[2]] = $p[1];
         $res[$p[0]
     }
 }
